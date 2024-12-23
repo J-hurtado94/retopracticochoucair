6476050516c782b4f1f72b995b1b04592fb5c111
Juan Carlos Hurtado Martínez <jhurtadom@choucairtesting.com>
2024-12-23 17:53:14 -0500

    Practica screenplay choucair

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -34,10 +34,11 @@ public class LoginStepDefinitions {
         String username= System.getenv("USER_NAME");
         String password= System.getenv("USER_PASSWORD");
 
-        usuarioData.stream().peek(
+        usuarioData.stream().map(
                  data -> {
                      data.setUsername(username);
                      data.setPassword(password);
+                     return data;
 
                  }).forEach( data ->
                 actor.attemptsTo(Login.sauceDemo(data.getUsername(), data.getPassword()))
@@ -56,23 +57,5 @@ public class LoginStepDefinitions {
 
 
 
-//
-//    @Given("{actor} is researching things on the internet")
-//    public void researchingThings(Actor actor) {
-//        actor.wasAbleTo(NavigateTo.theSearchHomePage());
-//    }
-//
-//    @When("{actor} looks up {string}")
-//    public void searchesFor(Actor actor, String term) {
-//        actor.attemptsTo(
-//                LookForInformation.about(term)
-//        );
-//    }
-//
-//    @Then("{actor} should see information about {string}")
-//    public void should_see_information_about(Actor actor, String term) {
-//        actor.attemptsTo(
-//                Ensure.that(TheWebPage.title()).containsIgnoringCase(term)
-//        );
-//    }
+
 }
